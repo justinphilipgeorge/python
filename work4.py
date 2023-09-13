5bc160914e89506d1318718a56deca602c1e469a
justin <justinphilip7710@gmail.com>
2023-09-13 00:46:50 -0700

    adding files

---
+++ work4.py
@@ -0,0 +1,16 @@
+a="coding" 
+print(a[3])
+print(a[0:4])
+print(a[0:])
+print(a[::-1])
+print(a[-1])
+
+b="Lorem ipsum is a placeholder text commonly used in the design and printing industry. It is a Latin looking text that has been used since the 16th century as a filler text when the final content is not yet available. Lorem ipsum is used to give an idea of the visual layout of a book, document or webpage without being distracted by the real content."
+print(len(b))
+
+print(a.upper())
+print(a.lower())
+print(a.replace("i","c"))
+print(a.startswith("c"))
+print(a.endswith("n"))
+
---
+++ work5.py
@@ -0,0 +1,12 @@
+# a=(input("Enter a value:"))
+# print(len(a))
+
+b="Zoo"
+print(b Number of 'o' characters in a string:{count})
+
+# c="pyt@on"
+# print(c.replace("@","h"))
+
+# d=input("Enter first string:")
+# e=input("Enter second string:")
+# print("Concatenated string:",d+e)
