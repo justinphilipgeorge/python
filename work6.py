f41d9aacdb331187eb9a4ace2a4dc65261f3cb92
justin <justinphilip7710@gmail.com>
2023-09-14 00:43:41 -0700

    adding files

---
+++ work6.py
@@ -0,0 +1,14 @@
+l=[1,2,3,4.5,"abc",2,2]
+print(l)
+
+
+l.append("ef")
+print(l)
+l.insert(2,"gg")
+print(l)
+l.remove(4.5)
+print(l)
+del l[2]
+print(l)
+l[1]=22
+print(l)
---
+++ work8.py
@@ -0,0 +1,19 @@
+tup=("cat","dog","goat")
+
+print(len(tup))
+
+ta=("apple","orange")
+th=(tup+ta)
+print(th)
+
+l=list(th)
+print(l)
+
+l.insert(5,"xyz")
+print(l)
+
+l.clear()
+print(l)
+
+del     l
+print(l)
